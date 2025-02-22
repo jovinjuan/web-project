3970bf464a92d3602a6987812416ba1e40848956
Faline <falinechen13@gmail.com>
2025-02-22 17:58:15 +0700

    Update navbar home.php

--- home.php
+++ home.php
@@ -3,7 +3,9 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Sistem Pelacak Kebiasaan Membaca Buku</title>
+    <title>Sistem</title>
+
+    <link rel="stylesheet" href="css/style.css" />
 
     <!-- BOOTSTRAP CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
@@ -13,37 +15,43 @@
   </head>
   <body>
     <!-- Nav Bar -->
-    <nav class="navbar navbar-expand-lg shadow p-3 fixed-top">
+    <nav class="navbar navbar-expand-lg bg-light shadow p-3 fixed-top">
       <div class="container">
         <i class="bi bi-box-fill"></i>
         <a class="navbar-brand" href="#">[Nama Sistem]</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+        <!-- Area Navigation -->
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="nav nav-pills">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link active" aria-current="page" href="home.php">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">My Progress</a>
+              <a class="nav-link" href="myprogress.php">My Progress</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Achievment</a>
+              <a class="nav-link" href="achievement.php">Achievement</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">History</a>
+              <a class="nav-link" href="history.php">History</a>
             </li>
           </ul>
         </div>
+        <!-- search bar -->
         <nav class="navbar bg-body-tertiary">
           <div class="container-fluid">
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button class="btn btn-outline-success" type="submit">Search</button>
+            <form class="d-flex ms-auto" role="search">
+              <input class="form-control me-2 w-80" type="search" placeholder="Search" aria-label="Search" />
+              <button class="btn btn-outline-primary" type="submit">Search</button>
             </form>
           </div>
         </nav>
+        <!-- profile button, kemudian ketika diklik maka akan beralih ke page settings-->
+        <a href="settings.php" class="btn btn-light">
+          <i class="bi bi-person-circle fs-5"></i>
+        </a>
       </div>
     </nav>
     <!-- Akhir Nav Bar -->
