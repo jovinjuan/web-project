49b36cdc8cbd1eca6f93dbf21f6785d588bf3d12
suvlim <suvanna0393@gmail.com>
2025-03-05 21:13:20 +0700

    Update history.php
    Rapikan Code

--- history.php
+++ history.php
@@ -334,9 +334,9 @@
               </div>
             </div>
           </div>
-
-            
-            </div>
+        </div>
+      </div>
+    </div>
             <!-- Akhir History -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
