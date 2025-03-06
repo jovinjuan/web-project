b7d0cfae9ad0afd3c46bbac2cfbdc552660de7d1
Faline <falinechen13@gmail.com>
2025-03-06 16:20:10 +0700

    Update Signup.php
    tambah name

--- Signup.php
+++ Signup.php
@@ -54,6 +54,7 @@
     <div class="Signup-box">
       <h2>Sign-Up</h2>
       <form onsubmit="redirectToHome(event)">
+        <input type="text" placeholder="Name" required>
         <input type="email" placeholder="Email Address" required />
         <input type="password" placeholder="Password" required />
         <button type="submit">Sign Up</button>
