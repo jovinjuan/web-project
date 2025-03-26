1ff4360e519ea0efae6b1475f14b20a1411093ab
suvlim <suvanna0393@gmail.com>
2025-03-27 00:15:13 +0700

    Update history.php navbar
    - Reuse Navbar.php file for simplicity

--- history.php
+++ history.php
@@ -129,61 +129,9 @@
 
 </head>
 <body>
-    <!-- Nav Bar -->
-    <nav class="navbar navbar-expand-lg bg-light shadow p-3 fixed-top">
-      <div class="container-fluid px-5">
-      <img src="treadyicon.png" alt="web icon" class = "mx-2" style = "width : 50px;  height : 63px;">
-        <a class="navbar-brand" href="#">Tready</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <!-- Area Navigation -->
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="nav nav-pills">
-            <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="home.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link " href="myprogress.php">My Progress</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="achievement.php">Achievement</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" href="history.php">History</a>
-            </li>
-          </ul>
-        </div>
-        <!-- search bar -->
-        <nav class="navbar bg-body-tertiary">
-          <div class="container-fluid">
-            <form class="d-flex ms-auto" role="search">
-              <input
-                class="form-control me-2 w-80"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button class="btn btn-outline-primary" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-        </nav>
-        <!-- profile button, kemudian ketika diklik maka akan beralih ke page settings-->
-        <a href="settings.php" class="btn btn-light">
-          <i class="bi bi-person-circle fs-5"></i>
-        </a>
-      </div>
-    </nav>
+   <!-- Nav Bar -->
+    <?php include 'Navbar.php';
+    ?>
     <!-- Akhir Nav Bar -->
     <!-- Streak -->
     <div class="container-fluid mt-5 pt-5">
